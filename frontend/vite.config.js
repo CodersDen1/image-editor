116c367d52ccd9820275f0965bab822061a775cf
Adbhut Mishra <106423660+CodersDen1@users.noreply.github.com>
2025-03-25 18:49:51 +0530

    [Update]: Vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,9 +5,18 @@ import tailwindcss from "tailwindcss";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Listen on all network interface   
+    strictPort: true // Ensure the specified port is used
+  },
   css: {
     postcss: {
       plugins: [tailwindcss()],
     },
   },
+  
+  preview: {
+    host: '0.0.0.0', // For preview mode as well
+    port: 4000
+  }
 });
