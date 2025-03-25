9b0f5995f3c94e6c199df872a11f9ea04afe5242
Átul J. <47384542+thisisaj1999@users.noreply.github.com>
2025-03-25 16:59:11 +0400

    [Update]: ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -20,11 +20,11 @@ module.exports = {
       watch: true,
       env: {
         NODE_ENV: "development",
-        PORT: 5555,
+        PORT: 3000,
       },
       env_production: {
         NODE_ENV: "production",
-        PORT: 5555,
+        PORT: 3000,
       },
     },
   ],
